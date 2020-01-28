f64c365dbb05ae1529daf847d045ff99f50f023c
sbx_user1051 <sbx_user1051@169.254.0.69>
2020-01-28 15:29:57 +0000

    Tue Jan 28 15:29:57 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,14 +10,39 @@ Price = int  # This should be decimal.Decimal if we wish to handle pennies
 class PricingInfo:
     def __init__(self) -> None:
         self.price_table: Dict[SKU, List[Tuple[int, Price]]] = {}
+        self.cart_discounts: Dict[SKU, Tuple[int, int, SKU]] = {}
 
     def add_sku(self, sku: SKU, unit_price: Price) -> None:
         if sku in self.price_table:
             raise KeyError(sku)
-        self.price_table[sku] = [(1, Price)]
+        self.price_table[sku] = [(1, unit_price)]
 
-    def add_multi_price_offer(sku: SKU, count: int, discount_price: Price) -> None:
+    def add_multi_price_offer(
+        self, sku: SKU, count: int, discount_price: Price
+    ) -> None:
+        # Assumes no conflicting offers
         self.price_table[sku].append((count, discount_price))
+        self.price_table[sku].sort(reverse=True)
+
+    def add_buy_many_get_some_free(self, x: SKU, n: int, y: SKU, m: int) -> None:
+        if x == y:
+            # This is a hidden multi-price discount
+            self.add_multi_price_offer(x, n + m, n * self.get_price(x, 1))
+        else:
+            self.cart_discounts[x] = (n, m, y)
+
+    def get_price(self, sku: SKU, count: int) -> Price:
+        """
+        Returns price using best offer for the given count of a product by sku
+        """
+        price = 0
+        # This depends on
+        #  - the offers being stable
+        #  - having a unit price at the end
+        for offer_count, offer_price in self.price_table[sku]:
+            total_offers, count = divmod(count, offer_count)
+            price += total_offers * offer_price
+        return price
 
 
 # Given an SKU, a list of possible ways to buy it (including by unit),
@@ -77,3 +102,4 @@ def checkout(skus: str) -> Price:
         except KeyError:
             return -1
     return total
+
