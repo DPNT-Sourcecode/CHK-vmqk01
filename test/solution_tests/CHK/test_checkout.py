2f6ddbd93ba845ee990d5f04a751daab78db95e7
sbx_user1051 <sbx_user1051@169.254.0.69>
2020-01-28 13:07:51 +0000

    Tue Jan 28 13:07:51 UTC 2020

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -25,6 +25,22 @@ def test_checkout_offer_mixed():
     assert checkout_solution.checkout("ABABAA") == 130+45+50
 
 
+def test_checkout_get_one_free_offer():
+    assert checkout_solution.checkout("EE") == 40*2
+    assert checkout_solution.checkout("EEB") == 40*2  # B is free
+
+
+def test_checkout_get_many_free_offer():
+    assert checkout_solution.checkout("EEEEBB") == 40*4  # 2xB are free
+
+
+def test_checkout_get_one_free_combined():
+    # In this scenario one B is free, the other pays unit price
+    assert checkout_solution.checkout("EEBB") == 40*2 + 30
+    # In this scenario one B is free, the other gets multi-price offer
+    assert checkout_solution.checkout("EEBBB") == 40*2 + 45
+
+
 def test_checkout_invalid():
     assert checkout_solution.checkout("X") == -1
 
@@ -39,4 +55,5 @@ def test_get_best_price():
 
 def test_get_best_price_no_offer():
     assert checkout_solution.get_best_price("D", 42) == 42*15
+
 
