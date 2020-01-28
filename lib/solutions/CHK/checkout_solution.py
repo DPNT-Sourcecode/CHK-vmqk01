b5c7b3ce2a6bd4226727acfdcbe53ec53a955eb5
sbx_user1051 <sbx_user1051@169.254.0.69>
2020-01-28 12:34:54 +0000

    Tue Jan 28 12:34:54 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,14 @@
 # noinspection PyUnusedLocal
+from typing import Dict
+
+SKU = str
+Price = int  # This should be decimal.Decimal if we wish to handle pennies
+PRICE_TABLE: Dict[SKU, Dict[int, Price]] = {
+    "A": {1: 50, 3: 130},
+    "B": {1: 30, 2: 45},
+    "C": {1: 20},
+    "D": {1: 15},
+}
 
 
 # skus = unicode string
---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,5 @@
+from solutions.CHK import checkout_solution
+
+
+def test_checkout():
+    assert 1==1
