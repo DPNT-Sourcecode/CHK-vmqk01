72f25aacab312558ed274d0805c4f778c99460d4
sbx_user1051 <sbx_user1051@169.254.0.69>
2020-01-28 12:55:54 +0000

    Tue Jan 28 12:55:54 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,11 @@ from typing import Dict
 
 SKU = str
 Price = int  # This should be decimal.Decimal if we wish to handle pennies
+
+# Given an SKU, a map of possible ways to buy it (including by unit), attaching
+# count to price
+# This allows for more offers. Could be a list of pairs (all we do on this dict
+# is iteration on key-value pairs), but it's slightly easier to read this way
 PRICE_TABLE: Dict[SKU, Dict[int, Price]] = {
     "A": {1: 50, 3: 130},
     "B": {1: 30, 2: 45},
@@ -14,9 +19,11 @@ PRICE_TABLE: Dict[SKU, Dict[int, Price]] = {
 
 
 def get_best_price(sku: SKU, count: int) -> Price:
-    """Returns price using best offer for the given count of a product by sku"""
+    """
+    Returns price using best offer for the given count of a product by sku
+    """
     unit_price = PRICE_TABLE[sku][1]  # This should always be present
-    best = math.inf
+    best: Price = math.inf  # type: ignore
     for offer_count, offer_price in PRICE_TABLE[sku].items():
         total_offers, remaining = (count // offer_count), (count % offer_count)
         best = min(best, total_offers * offer_price + remaining * unit_price)
@@ -30,3 +37,4 @@ def checkout(skus: str) -> Price:
     for sku, count in counts.items():
         total += get_best_price(sku, count)
     return total
+
