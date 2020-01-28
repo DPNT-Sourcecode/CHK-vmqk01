eeaf3d0ba621bb267119b0dc4e68a6399991937e
sbx_user1051 <sbx_user1051@169.254.0.69>
2020-01-28 12:37:54 +0000

    Tue Jan 28 12:37:54 UTC 2020

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,5 +1,15 @@
 from solutions.CHK import checkout_solution
 
 
-def test_checkout():
-    assert 1==1
+def test_checkout_single():
+    assert checkout_solution.checkout("A") == 50
+
+
+def test_checkout_empty():
+    assert checkout_solution.checkout("") == 0
+
+
+def test_checkout_multiple():
+    assert checkout_solution.checkout("ABCD") == 50+30+20+15
+
+
