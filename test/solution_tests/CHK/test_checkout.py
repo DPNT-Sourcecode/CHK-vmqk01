460b03d53379c9047e20825eb43b983fd67fceb5
sbx_user1051 <sbx_user1051@169.254.0.69>
2020-01-28 13:34:25 +0000

    Tue Jan 28 13:34:25 UTC 2020

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -81,3 +81,48 @@ def test_apply_discounts_no_free_items_asked():
     assert cart["B"] == 0
 
 
+def test_external_tests():
+    # These came from my first deploy run
+    assert checkout_solution.checkout("") == 0
+    assert checkout_solution.checkout("A") == 50
+    assert checkout_solution.checkout("B") == 30
+    assert checkout_solution.checkout("C") == 20
+    assert checkout_solution.checkout("D") == 15
+    assert checkout_solution.checkout("E") == 40
+    assert checkout_solution.checkout("a") == -1
+    assert checkout_solution.checkout("-") == -1
+    assert checkout_solution.checkout("ABCa") == -1
+    assert checkout_solution.checkout("AxA") == -1
+    assert checkout_solution.checkout("ABCDE") == 155
+    assert checkout_solution.checkout("A") == 50
+    assert checkout_solution.checkout("AA") == 100
+    assert checkout_solution.checkout("AAA") == 130
+    assert checkout_solution.checkout("AAAA") == 180
+    assert checkout_solution.checkout("AAAAA") == 200
+    assert checkout_solution.checkout("AAAAAA") == 250
+    assert checkout_solution.checkout("AAAAAAA") == 300
+    assert checkout_solution.checkout("AAAAAAAA") == 330
+    assert checkout_solution.checkout("AAAAAAAAA") == 380
+    assert checkout_solution.checkout("AAAAAAAAAA") == 400
+    assert checkout_solution.checkout("EE") == 80
+    assert checkout_solution.checkout("EEB") == 80
+    assert checkout_solution.checkout("EEEB") == 150
+    assert checkout_solution.checkout("EEEEBB") == 205
+    assert checkout_solution.checkout("BEBEEE") == 205
+    assert checkout_solution.checkout("A") == 50
+    assert checkout_solution.checkout("AA") == 100
+    assert checkout_solution.checkout("AAA") == 130
+    assert checkout_solution.checkout("AAAA") == 180
+    assert checkout_solution.checkout("AAAAA") == 200
+    assert checkout_solution.checkout("AAAAAA") == 250
+    assert checkout_solution.checkout("B") == 30
+    assert checkout_solution.checkout("BB") == 45
+    assert checkout_solution.checkout("BBB") == 75
+    assert checkout_solution.checkout("BBBB") == 90
+    assert checkout_solution.checkout("ABCDEABCDE") == 295
+    assert checkout_solution.checkout("CCADDEEBBA") == 295
+    assert checkout_solution.checkout("AAAAAEEBAAABB") == 505
+    assert checkout_solution.checkout("ABCDECBAABCABBAAAEEAA") == 705
+
+
+
